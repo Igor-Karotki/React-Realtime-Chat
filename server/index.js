edfaed9fbed0edcc34424e1b97e0b1a264169228
Igor Karotki <igorkarotki@gmail.com>
2020-09-01 16:14:42 +0200

    01.09 16.14

--- server/index.js
+++ server/index.js
@@ -4,8 +4,21 @@ const socketio = require('socket.io');
 
 const PORT = process.env.PORT || 5000;
 
+const router = require('./router');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+app.use(router);
+
+io.on('connect', (socket) => {
+ console.log('New connection')
+ socket.on('disconnect', () => {
+  console.log('USer had left')
+ });
+})
+
+app.user(router);
 
 server.listen(PORT, () => console.log(`Server has started. ${PORT}`));
